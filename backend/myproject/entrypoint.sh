b852307cdec51a9f34d4bb0115479b59683de3ff
TheDrakl <denysmelnyk262626@gmail.com>
2024-11-28 18:04:51 +0000

    Fixed docker compose for backend

--- backend/myproject/entrypoint.sh
+++ backend/myproject/entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
@@ -14,7 +14,10 @@ fi
 echo "Applying database migration"
 python manage.py migrate
 
-echo "Loading data from fixtures"
-python manage.py loaddata fixtures/initial_data.json
-
+if [ -f fixtures/initial_data.json ]; then
+    echo "Loading data from fixtures"
+    python manage.py loaddata fixtures/initial_data.json
+else
+    echo "Fixture file does not exist"
+fi
 exec "$@"
