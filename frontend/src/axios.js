2f3aad40f5fa1cae5f3cfdcab0560565f24407db
TheDrakl <denysmelnyk262626@gmail.com>
2024-11-29 21:34:56 +0000

    Test

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://backend-em05.onrender.com/api/',
-  headers: {
-    'X-API-KEY': `Bearer ${process.env.VUE_APP_SECRET_API_KEY}`,  
-  }
+  // headers: {
+  //   'X-API-KEY': `Bearer ${process.env.VUE_APP_SECRET_API_KEY}`,  
+  // }
 });
 
 export default api;
