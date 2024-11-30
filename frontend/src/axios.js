a5e1e433097b60adbc2e4e0412ef79fa55dd3a57
TheDrakl <denysmelnyk262626@gmail.com>
2024-11-30 10:53:15 +0000

    test

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://backend-em05.onrender.com/api/',
   headers: {
-    'X-API-KEY': import.meta.env.VITE_APP_SECRET_API_KEY,  
+    'X-API-KEY': `${import.meta.env.VITE_APP_SECRET_API_KEY}`,
+    'Content-Type': 'application/json', 
   }
 });
 
